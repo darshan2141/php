4ecb0627e03a0615a82d8b276fa370afadde774c
darshan2141 <ubhadiyadarshan@gmail.com>
2023-02-26 17:26:13 +0530

    first commit

---
+++ formHandling1.php
@@ -0,0 +1,8 @@
+<?php
+
+?>
+<form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
